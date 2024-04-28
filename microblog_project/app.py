3b991081aeb0f7f79686369e934ace7c81333c06
Arthur Mateu <arthurmateu@gmail.com>
2024-04-28 18:02:52 +0200

    App factory

--- microblog_project/app.py
+++ microblog_project/app.py
@@ -2,23 +2,26 @@ import datetime
 from flask import Flask, render_template, request
 from pymongo import MongoClient
 
-app = Flask(__name__)
-client = MongoClient("mongodb+srv://arthurmateu:ylIaOUyqHnvfobNH@microblog-uwdbfp.e0whze4.mongodb.net/?retryWrites=true&w=majority&appName=microblog-uwdbfp")
-app.db = client.microblog
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient("mongodb+srv://arthurmateu:ylIaOUyqHnvfobNH@microblog-uwdbfp.e0whze4.mongodb.net/?retryWrites=true&w=majority&appName=microblog-uwdbfp")
+    app.db = client.microblog
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST':
-        entry_content = request.form.get("content")
-        formatted_date = datetime.datetime.today().strftime("%d/%m/%Y")
-        app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+    @app.route('/', methods=['GET', 'POST'])
+    def home():
+        if request.method == 'POST':
+            entry_content = request.form.get("content")
+            formatted_date = datetime.datetime.today().strftime("%d/%m/%Y")
+            app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
 
-    entries_with_date = [
-        (
-            entry["content"],
-            entry["date"],
-            datetime.datetime.strptime(entry["date"], "%d/%m/%Y").strftime("%b %d")
-        )
-        for entry in app.db.entries.find({})
-    ]
-    return render_template("home.html", entries=entries_with_date)
+        entries_with_date = [
+            (
+                entry["content"],
+                entry["date"],
+                datetime.datetime.strptime(entry["date"], "%d/%m/%Y").strftime("%b %d")
+            )
+            for entry in app.db.entries.find({})
+        ]
+        return render_template("home.html", entries=entries_with_date)
+
+    return app
