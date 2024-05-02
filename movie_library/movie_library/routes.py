5083817861db11ee3d365c35abdf9b0c652a4f70
Arthur Mateu <arthurmateu@gmail.com>
2024-05-02 20:03:29 +0200

    Dark mode support and starting to add WTForms

--- movie_library/movie_library/routes.py
+++ movie_library/movie_library/routes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, session, redirect, request
+from movie_library.forms import MovieForm
 
 pages = Blueprint(
     "pages", __name__, template_folder="templates", static_folder="static"
@@ -12,3 +12,26 @@ def index():
         "index.html",
         title="Movies Watchlist",
     )
+
+
+@pages.route("/add", methods=["GET", "POST"])
+def add_movie():
+    form = MovieForm()
+
+    if request.method == "POST":
+        pass
+
+    return render_template(
+        "new_movie.html", title="Movies Watchlist - Add Movie", form=form
+    )
+
+
+@pages.get("/toggle-theme")
+def toggle_theme():
+    current_theme = session.get("theme")
+    if current_theme == "dark":
+        session["theme"] = "light"
+    else:
+        session["theme"] = "dark"
+
+    return redirect(request.args.get("current_page"))
